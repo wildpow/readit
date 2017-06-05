d2a12a7a82903c7e9086fea98c7cd96be27c1c66
wildpow <element1028@gmail.com>
2017-06-04 23:31:26 -0700

    page 190

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,14 @@
 class StoriesController < ApplicationController
   def index
-    @story = Story.find_by(name: 'SitePoint Forums')
+    @story = Story.order('RANDOM()').first
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(params[:story])
+    @story.save
   end
 end
